07564b17ea8a5e07ebd4982d7b21b74e68019877
Hironori Okano <okano@west.sd.keio.ac.jp>
2013-06-12 14:13:26 +0900

    add list

--- Numeron/numeron.cpp
+++ Numeron/numeron.cpp
@@ -2,8 +2,10 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <list>
+#include <functional>
 
-//#define DEBUG
+#define DEBUG
 using namespace std;
 
 bool isValidNumber(int a);
@@ -24,17 +26,17 @@ public:
 class Player{
 private:
 	Entry answer;
-	Entry anslist[20];
 	int count;
 public:
-	void setLastEntry(Entry test){this->anslist[count]=test;}
+	list<Entry> anslist;
+	//void setLastEntry(Entry test){this->anslist[count]=test;}
 	void incCount(){this->count++;}
 	int getCount(){return count;}
 	void setAnswer(int ans){this->answer.number=ans;}
 	int getAns(){return answer.number;}
 	void viewAnsList(){
-		for(int i=0;i<count;i++){
-			cout<<i<<" : "<<anslist[i].number<<"="<<anslist[i].eat<<"-"<<anslist[i].bite<<endl;
+		for(list<Entry>::iterator it = anslist.begin(); it!=anslist.end(); it++){
+			cout<<" : "<<it->number<<"="<<it->eat<<"-"<<it->bite<<endl;
 		}
 	}
 	Entry respond(int test){
@@ -58,7 +60,6 @@ public:
 class Computer : public Player{
 private:
 	Entry entry[1000];
-	int asked[30];
 	int candidates;
 public:
 	void setEntry(Entry a){entry[a.number]=a;}
@@ -69,6 +70,16 @@ public:
 	void think(){
 		//とりあえず、今は何も考えていない
 		//候補を削除するだけで十分強い。
+		//良い質問を「多くの候補を削除できる質問」と定義する。
+		for(int i=0;i<1000;i++){
+			if(entry[i].probablity>0){
+				//iを質問したとき。
+//				for(){
+					//削除される場合。
+	//				if()entry[i].probablity++;
+		//		}
+			}
+		}
 	}
 	void remove(int test,int eat,int bite){
 		int m;
@@ -107,7 +118,8 @@ public:
 				cout<<entry[i].number<<",";
 				flag=true;
 			}else{
-				//cout<<",";
+				cout<<"___,";
+				flag=true;
 			}
 			if(i%10==0 && flag==true){
 				cout<<endl;
@@ -118,6 +130,7 @@ public:
 		cout<<"The number of candidates is "<<candidates<<endl;
 	}
 	int select(){
+		/*
 		//select in random from candidates.
 		srand( (unsigned)time( NULL ) );
 		int count2=rand()%500;
@@ -130,6 +143,16 @@ public:
 			}
 		}while(count2>0);
 		return 123;
+		*/
+		int max_probability=0;
+		int cand;
+		for(int i=0;i<1000;i++){
+			if(entry[i].probablity>max_probability){
+				max_probability=entry[i].probablity;
+				cand=i;
+			}
+		}
+		return cand;
 	}
 	Computer(){
 		candidates=0;
@@ -180,6 +203,7 @@ int main(){
 		cout<<"OK!Start!Numeron."<<endl;
 		//Start!!
 		do{
+			turn=!turn;
 			if(turn){
 				//player
 				cout<<"<<PLAYER ("<<player.getCount()<<")>>"<<endl;
@@ -189,7 +213,8 @@ int main(){
 					cin>>sample.number;
 				}while(isValidNumber(sample.number)==false);
 				sample=computer.respond(sample.number);
-				player.setLastEntry(sample);
+				//player.setLastEntry(sample);
+				player.anslist.insert(player.anslist.end(),sample);
 				player.incCount();
 				cout<<"(computer)"<<sample.number<<"is"<<sample.eat<<"-"<<sample.bite<<endl;
 			}else{
@@ -205,13 +230,26 @@ int main(){
 				}while(isValidNumber(sample.number)==false);
 				sample=player.respond(sample.number);
 				cout<<"(player)"<<sample.number<<"is"<<sample.eat<<"-"<<sample.bite<<endl;
-				computer.setLastEntry(sample);
+				computer.anslist.insert(computer.anslist.end(),sample);
+				//computer.setLastEntry(sample);
 				computer.incCount();
 				computer.setEntry(sample);
 				computer.Proc(sample);
 			}
-			turn=!turn;
 		}while(sample.eat!=3);
+		if(turn){
+			cout<<"# #  #  # #  # # # ### #  # ##"<<endl;
+			cout<<"# # # # # #  # # #  #  ## # ##"<<endl;
+			cout<<"### # # # #  # # #  #  #### ##"<<endl;
+			cout<<" #  # # # #  ## ##  #  # ##   "<<endl;
+			cout<<" #   #   #    # #  ### #  # ##"<<endl;
+		}else{
+			cout<<"# #  #  # #  #    #   ## ###"<<endl;
+			cout<<"# # # # # #  #   # # #   #  "<<endl;
+			cout<<"### # # # #  #   # #  #  ###"<<endl;
+			cout<<" #  # # # #  #   # #   # #  "<<endl;
+			cout<<" #   #   #   ###  #  ##  ###"<<endl;
+		}
 		cout<<"<<<END>>> : count="<< computer.getCount()<<endl;
 		cout<<"computer's number is"<<computer.getAns()<<endl; 
 		cout<<"player's number is"<<player.getAns()<<endl; 
